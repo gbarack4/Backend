eb251ba3374042b08e231d23af64aefb04053691
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2026-05-28 16:02:04 +0300

    feat: sync clerk users with local database
    - Add WebhooksController to process Clerk user.created events
    - Implement Svix signature validation
    - Register WebhooksController in UsersModule
    - Update .env.example with CLERK_WEBHOOK_SECRET

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { WebhooksController } from './webhooks.controller';
 
 @Module({
-  controllers: [UsersController],
+  controllers: [UsersController, WebhooksController],
   providers: [UsersService],
 })
 export class UsersModule {}
