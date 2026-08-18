2528c27b89b4f76f45fc004e6ed43ef95c25c0e9
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2026-08-18 12:03:12 +0300

    fix: correct google reviews api domain to v4

--- src/google/constants/google.endpoints.ts
+++ src/google/constants/google.endpoints.ts
@@ -2,8 +2,10 @@ export const GOOGLE_ENDPOINTS = {
   ACCOUNTS: 'https://mybusinessaccountmanagement.googleapis.com/v1/accounts',
   LOCATIONS: (accountName: string) =>
     `https://mybusinessbusinessinformation.googleapis.com/v1/${accountName}/locations?readMask=name,title`,
+
   REVIEWS: (accountName: string, locationName: string) =>
-    `https://mybusinessreviews.googleapis.com/v1/${accountName}/${locationName}/reviews`,
+    `https://mybusiness.googleapis.com/v4/${accountName}/${locationName}/reviews`,
+
   GEOCODING: 'https://maps.googleapis.com/maps/api/geocode/json',
   TIMEZONE: 'https://maps.googleapis.com/maps/api/timezone/json',
 } as const;
